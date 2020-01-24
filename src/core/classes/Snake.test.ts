f4d3817bea54ba6b43db7cc1bc2aef48e3483705
Vladyslav Kazantsev <specialistvlad@gmail.com>
2020-01-24 11:08:23 +0530

    preparing for new tests

--- src/core/classes/Snake.test.ts
+++ src/core/classes/Snake.test.ts
@@ -1,5 +1,5 @@
 import { Snake, TSnakeConstructorParams } from './Snake';
-import { TGameState, TDegree, Directions, CellType } from '../types';
+import { TGameState, TDegree, Directions, TCoordinates } from '../types';
 
 type optionalDegree = TDegree | null;
 
@@ -16,143 +16,146 @@ const makeSnakeStepsReducer = (snake: Snake) =>
 // template for each test for reducers
 const makeReducerTest = (
     steps: Array<optionalDegree>,
-    snakeParams: TSnakeConstructorParams
+    snake: TCoordinates,
+    params?: any, 
     ) =>
     // @ts-ignore
-    [null, ...steps].reduce(makeSnakeStepsReducer(new Snake(snakeParams)), defaultState);
+    [null, ...steps]
+        .reduce(makeSnakeStepsReducer(new Snake({ name: '', initPoint: snake[0], ...params })), defaultState);
+
+const matchCoordinates = (coordinate: TCoordinates) => ({ cells: coordinate.map(item => (expect.objectContaining({ coordinate: item })))});
 
 describe('Snake', () => {
     test('snake init step works correctly(without move forward, only rendering)', () =>
-        expect(makeReducerTest([], { name: '', initPoint: [0, 0]}))
-            .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 0] })]}));
+        expect(makeReducerTest([], [[0, 0]])).toMatchObject(matchCoordinates([[0, 0]])));
 
     describe('move', () => {
         describe('from default direction to', () => {
             test('default direction', () =>
-            expect(makeReducerTest([null], { name: '', initPoint: [1, 1]}))
+            expect(makeReducerTest([null], [[1, 1]]))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
             
             test('default to Right', () =>
-            expect(makeReducerTest([Directions.Right], { name: '', initPoint: [1, 1]}))
+            expect(makeReducerTest([Directions.Right], [[1, 1]]))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
             
             test('default to Down', () =>
-            expect(makeReducerTest([Directions.Down], { name: '', initPoint: [1, 1]}))
+            expect(makeReducerTest([Directions.Down], [[1, 1]]))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [2, 1] })]}));
     
             test('default to Left', () =>
-            expect(makeReducerTest([Directions.Left], { name: '', initPoint: [1, 1]}))
+            expect(makeReducerTest([Directions.Left], [[1, 1]]))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
     
             test('default to Up', () =>
-            expect(makeReducerTest([Directions.Up], { name: '', initPoint: [1, 1]}))
+            expect(makeReducerTest([Directions.Up], [[1, 1]]))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 1] })]}));
         });
 
         describe('from Right to', () => {
             test('default direction', () =>
-            expect(makeReducerTest([null], { name: '', initPoint: [1, 1]}))
+            expect(makeReducerTest([null], [[1, 1]]))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
             
             test('Right', () =>
-            expect(makeReducerTest([Directions.Right], { name: '', initPoint: [1, 1], direction: Directions.Right}))
+            expect(makeReducerTest([Directions.Right], [[1, 1]], { direction: Directions.Right }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
             
             test('Down', () =>
-            expect(makeReducerTest([Directions.Down], { name: '', initPoint: [1, 1], direction: Directions.Right}))
+            expect(makeReducerTest([Directions.Down], [[1, 1]], { direction: Directions.Right }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [2, 1] })]}));
     
             test('Left', () =>
-            expect(makeReducerTest([Directions.Left], { name: '', initPoint: [1, 1], direction: Directions.Right}))
+            expect(makeReducerTest([Directions.Left], [[1, 1]], { direction: Directions.Right }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
     
             test('Up', () =>
-            expect(makeReducerTest([Directions.Up], { name: '', initPoint: [1, 1], direction: Directions.Right}))
+            expect(makeReducerTest([Directions.Up], [[1, 1]], { direction: Directions.Right }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 1] })]}));
         });
 
         describe('from Down direction to', () => {
             test('default direction', () =>
-            expect(makeReducerTest([null], { name: '', initPoint: [1, 1], direction: Directions.Down}))
+            expect(makeReducerTest([null], [[1, 1]], { direction: Directions.Down }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [2, 1] })]}));
             
             test('Right', () =>
-            expect(makeReducerTest([Directions.Right], { name: '', initPoint: [1, 1], direction: Directions.Down}))
+            expect(makeReducerTest([Directions.Right], [[1, 1]], { direction: Directions.Down }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
             
             test('Down', () =>
-            expect(makeReducerTest([Directions.Down], { name: '', initPoint: [1, 1], direction: Directions.Down}))
+            expect(makeReducerTest([Directions.Down], [[1, 1]], { direction: Directions.Down }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [2, 1] })]}));
     
             test('Left', () =>
-            expect(makeReducerTest([Directions.Left], { name: '', initPoint: [1, 1], direction: Directions.Down}))
+            expect(makeReducerTest([Directions.Left], [[1, 1]], { direction: Directions.Down }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 0] })]}));
     
             test('Up', () =>
-            expect(makeReducerTest([Directions.Up], { name: '', initPoint: [1, 1], direction: Directions.Down}))
+            expect(makeReducerTest([Directions.Up], [[1, 1]], { direction: Directions.Down }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [2, 1] })]}));
         });
 
         describe('from Left direction to', () => {
             test('default direction', () =>
-            expect(makeReducerTest([null], { name: '', initPoint: [1, 1], direction: Directions.Left}))
+            expect(makeReducerTest([null], [[1, 1]], { direction: Directions.Left }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 0] })]}));
             
             test('Right', () =>
-            expect(makeReducerTest([Directions.Right], { name: '', initPoint: [1, 1], direction: Directions.Left}))
+            expect(makeReducerTest([Directions.Right], [[1, 1]], { direction: Directions.Left }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 0] })]}));
             
             test('Down', () =>
-            expect(makeReducerTest([Directions.Down], { name: '', initPoint: [1, 1], direction: Directions.Left}))
+            expect(makeReducerTest([Directions.Down], [[1, 1]], { direction: Directions.Left }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [2, 1] })]}));
     
             test('Left', () =>
-            expect(makeReducerTest([Directions.Left], { name: '', initPoint: [1, 1], direction: Directions.Left}))
+            expect(makeReducerTest([Directions.Left], [[1, 1]], { direction: Directions.Left }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 0] })]}));
     
             test('Up', () =>
-            expect(makeReducerTest([Directions.Up], { name: '', initPoint: [1, 1], direction: Directions.Left}))
+            expect(makeReducerTest([Directions.Up], [[1, 1]], { direction: Directions.Left }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 1] })]}));
         });
 
         describe('from Up direction to', () => {
             test('default direction', () =>
-            expect(makeReducerTest([null], { name: '', initPoint: [1, 1], direction: Directions.Up}))
+            expect(makeReducerTest([null], [[1, 1]], { direction: Directions.Up }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 1] })]}));
             
             test('Right', () =>
-            expect(makeReducerTest([Directions.Right], { name: '', initPoint: [1, 1], direction: Directions.Up}))
+            expect(makeReducerTest([Directions.Right], [[1, 1]], { direction: Directions.Up }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 2] })]}));
             
             test('Down', () =>
-            expect(makeReducerTest([Directions.Down], { name: '', initPoint: [1, 1], direction: Directions.Up}))
+            expect(makeReducerTest([Directions.Down], [[1, 1]], { direction: Directions.Up }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 1] })]}));
     
             test('Left', () =>
-            expect(makeReducerTest([Directions.Left], { name: '', initPoint: [1, 1], direction: Directions.Up}))
+            expect(makeReducerTest([Directions.Left], [[1, 1]], { direction: Directions.Up }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [1, 0] })]}));
     
             test('Up', () =>
-            expect(makeReducerTest([Directions.Up], { name: '', initPoint: [1, 1], direction: Directions.Up}))
+            expect(makeReducerTest([Directions.Up], [[1, 1]], { direction: Directions.Up }))
                 .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 1] })]}));
         });
     });
 
     describe('move through the mirror', () => {
         test('default to Right', () =>
-        expect(makeReducerTest([null], { name: '', initPoint: [19, 19], tableSize: 20, direction: Directions.Right}))
+        expect(makeReducerTest([null], [[19, 19]], { direction: Directions.Right, tableSize: 20 }))
             .toMatchObject({ cells: [expect.objectContaining({ coordinate: [19, 0] })]}));
         
         test('default to Down', () =>
-        expect(makeReducerTest([null], { name: '', initPoint: [20, 20], tableSize: 21, direction: Directions.Down }))
+        expect(makeReducerTest([null], [[20, 20]], { direction: Directions.Down, tableSize: 21 }))
             .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 20] })]}));
 
         test('default to Left  with default table size', () =>
-        expect(makeReducerTest([null], { name: '', initPoint: [0, 0], direction: Directions.Left}))
+        expect(makeReducerTest([null], [[0, 0]], { direction: Directions.Left }))
             .toMatchObject({ cells: [expect.objectContaining({ coordinate: [0, 99] })]}));
 
         test('default to Up', () =>
-        expect(makeReducerTest([null], { name: '', initPoint: [0, 0], tableSize: 20, direction: Directions.Up}))
+        expect(makeReducerTest([null], [[0, 0]], { direction: Directions.Up, tableSize: 20 }))
             .toMatchObject({ cells: [expect.objectContaining({ coordinate: [19, 0] })]}));
     });
 });
