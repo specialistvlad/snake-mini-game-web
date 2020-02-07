47e107cff560cfb374498434d789b670d9411698
Vladyslav Kazantsev <specialistvlad@gmail.com>
2020-02-07 09:39:48 +0530

    Updates

---
+++ precache-manifest.cd726a0584fe7e0a7260f2f93fec5051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d668a04a872cbc5fb5cdecbda273ae7b",
+    "url": "/snake-ml-experiment/index.html"
+  },
+  {
+    "revision": "7a70489a19126a977bfe",
+    "url": "/snake-ml-experiment/static/css/main.0a74afbc.chunk.css"
+  },
+  {
+    "revision": "2ada971e984247720e61",
+    "url": "/snake-ml-experiment/static/js/2.56375214.chunk.js"
+  },
+  {
+    "revision": "affb0a5817905366c069622cab22b6df",
+    "url": "/snake-ml-experiment/static/js/2.56375214.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a70489a19126a977bfe",
+    "url": "/snake-ml-experiment/static/js/main.e5970d30.chunk.js"
+  },
+  {
+    "revision": "95bfc93c39b69231117d",
+    "url": "/snake-ml-experiment/static/js/runtime-main.b6524fc2.js"
+  }
+]);
