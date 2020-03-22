6376ad18fe7f84c2f147edddc21fafc684aaca24
Vladyslav Kazantsev <specialistvlad@gmail.com>
2020-03-22 11:02:54 +0530

    update test

--- src/core/classes/BaseAgent.test.ts
+++ src/core/classes/BaseAgent.test.ts
@@ -14,10 +14,25 @@ describe('Agent', () => {
                 { "coordinate": [0, 3], "type": 4 }, // snake head
                 { "coordinate": [0, 2], "type": 3 }, // snake
             ],
+        },{
+            "cells": [
+                { "coordinate": [2, 1], "type": 2 }, // food
+                { "coordinate": [0, 3], "type": 4 }, // snake head
+                { "coordinate": [0, 2], "type": 3 }, // snake
+                { "coordinate": [0, 1], "type": 3 }, // snake
+                { "coordinate": [0, 0], "type": 3 }, // snake
+                { "coordinate": [1, 0], "type": 3 }, // snake
+            ],
         }]).arraySync()).toMatchObject([
             [
                 [[0, 0], [0, 0], [1, 0], [2, 0]],
                 [[0, 0], [0, 0], [0, 0], [0, 0]],
+                [[0, 0], [0, 1], [0, 0], [0, 0]],
+                [[0, 0], [0, 0], [0, 0], [0, 0]],
+            ],
+            [
+                [[1, 0], [1, 0], [1, 0], [2, 0]],
+                [[1, 0], [0, 0], [0, 0], [0, 0]],
                 [[0, 0], [0, 1], [0, 0], [0, 0]],
                 [[0, 0], [0, 0], [0, 0], [0, 0]],
             ]
